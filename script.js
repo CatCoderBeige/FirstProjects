838c7435f07eda970b09c90e2850c58b30a5be5a
CatCoderBeige <christopherluca.fischer@gmail.com>
2025-07-03 16:33:30 +0000

    updated images

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function uncensor() {
 
   if (el) { // Überprüfe, ob das Element existiert
     el.innerHTML = `
-      <img class="Collage" src="IMG_0481.png">
-      <img class="Collage" src="IMG_0482.png">
-      <img class="Collage" src="IMG_0484.png">
-      <img class="Collage" src="0C597558-7435-4FCF-BF13-187183C915FE.jpeg">
-      <img class="Collage" src="40D5A531-8C99-470C-BEDC-83E13CF6A68E.jpeg">
-      <img class="Collage" src="C55E4DE5-93C2-40AE-8A17-C83BA86A2143.jpeg">
+      <img class="Collage" src="1.png">
+      <img class="Collage" src="2.png">
+      <img class="Collage" src="3.png">
+      <img class="Collage" src="4.jpeg">
+      <img class="Collage" src="5.jpeg">
+      <img class="Collage" src="6.jpeg">
       <a class="next" href="text.html">WILLST DU MEHR?</a>
     `;
   } else {
